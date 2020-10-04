3956abb1e760b4353769b772d7ca8ba72324713c
Andrew McDowell <madoliole@gmail.com>
2020-10-05 00:11:09 +1100

    Adding a debug flag

--- main.go
+++ main.go
@@ -6,17 +6,18 @@ import (
 
 type FilesInfo []FileInfo
 
-func initFlags() (string, string, string, string) {
+func initFlags() (string, string, string, string, bool) {
 	extensionsRegex := flag.String("extensionsRegex", "\\.(js|jsx|ts|jsx)$", "A regex for specifying file extensions")
 	directory := flag.String("directory", "./", "The directory to search")
 	exclusionsRegex := flag.String("exclusionsRegex", "(__snapshots__|__tests__|test\\\\.(js|ts|jsx|tsx))", "Any other files you dont want to include")
 	outputType := flag.String("outputType", "csv", "Choose between csv or json")
+	debug := flag.Bool("debug", false, "Turn on to log file info")
 	flag.Parse()
-	return *extensionsRegex, *directory, *exclusionsRegex, *outputType
+	return *extensionsRegex, *directory, *exclusionsRegex, *outputType, *debug
 }
 
 func main() {
-	extensionsRegex, directory, exclusionsRegex, outputType := initFlags()
+	extensionsRegex, directory, exclusionsRegex, outputType, debug := initFlags()
 
 	filePaths, err := getFilePathsForDirectory(directory, extensionsRegex, exclusionsRegex)
 
@@ -29,7 +30,9 @@ func main() {
 	for _, file := range filePaths {
 		fileInfo := complexityCounter(file)
 		files = append(files, fileInfo)
-		printFileInfo(fileInfo)
+		if debug == true {
+			printFileInfo(fileInfo)
+		}
 	}
 
 	if outputType == "csv" {
