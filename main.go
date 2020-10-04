dd45eb982ea75331267ebde0afcf317bf0d02a75
Andrew McDowell <madoliole@gmail.com>
2020-10-04 22:59:35 +1100

    Update the default extensions regex to make sure the file extensions are on the end

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"flag"
 )
+
 var extensionsRegex string
 
+type FilesInfo []FileInfo
+
 func initFlags() (string, string, string, string) {
-	extensionsRegex := flag.String("extensionsRegex", "\\.(js|jsx|ts|jsx)", "A regex for specifying file extensions")
+	extensionsRegex := flag.String("extensionsRegex", "\\.(js|jsx|ts|jsx)$", "A regex for specifying file extensions")
 	directory := flag.String("directory", "./", "The directory to search")
 	exclusionsRegex := flag.String("exclusionsRegex", "(__tests__|test\\.(js|ts|jsx|tsx))|__snapshots__", "Any other files you dont want to include")
 	outputType := flag.String("outputType", "csv", "Choose between csv or json")
@@ -23,7 +26,7 @@ func main() {
 		panic(err)
 	}
 
-	var files []FileInfo
+	var files FilesInfo
 
 	for _, file := range filePaths {
 		fileInfo := complexityCounter(file)
@@ -36,5 +39,7 @@ func main() {
 
 	if outputType == "csv" {
 		exportCsv(files, "./output.csv")
+	} else if outputType == "json" {
+		exportJson(files, "./output.json")
 	}
 }
