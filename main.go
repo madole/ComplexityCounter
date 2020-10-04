ff99ea6f522f561eef716fc59b14e25162b749ba
Andrew McDowell <madoliole@gmail.com>
2020-10-04 22:08:46 +1100

    Adding conditional flag to check for output type"

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 )
 var extensionsRegex string
 
-func initFlags() (string, string, string) {
+func initFlags() (string, string, string, string) {
 	extensionsRegex := flag.String("extensionsRegex", "\\.(js|jsx|ts|jsx)", "A regex for specifying file extensions")
 	directory := flag.String("directory", "./", "The directory to search")
 	exclusionsRegex := flag.String("exclusionsRegex", "(__tests__|test\\.(js|ts|jsx|tsx))|__snapshots__", "Any other files you dont want to include")
+	outputType := flag.String("outputType", "csv", "Choose between csv or json")
 	flag.Parse()
-	return *extensionsRegex, *directory, *exclusionsRegex
+	return *extensionsRegex, *directory, *exclusionsRegex, *outputType
 }
 
 func main() {
-	extensionsRegex, directory, exclusionsRegex := initFlags()
+	extensionsRegex, directory, exclusionsRegex, outputType := initFlags()
 
 	filePaths, err := getFilePathsForDirectory(directory, extensionsRegex, exclusionsRegex)
 
@@ -34,5 +35,8 @@ func main() {
 
 		//printFileInfo(fileInfo)
 	}
-	exportCsv(files, "./output.csv")
+
+	if outputType == "csv" {
+		exportCsv(files, "./output.csv")
+	}
 }
